3eba4d6e300a622c6b85a12e4487c533e231938c
djedje <zakodjedje@gmail.com>
2025-07-13 19:14:53 +0200

    test du bon code de vérifycation server request methode avec integration de json et des renvoi vers le front

--- index.php
+++ index.php
@@ -2,7 +2,14 @@
 
 require_once 'connexion.php';
 require_once 'user.php';
+header("Content-Type: application/json");
 
-addUser ($pdo,"djedje", "zako","zakodjedje@gmail.com", "dizafoya", "chauffeur" );
+$errors =[];
+
+if ($_SERVER ["REQUEST_METHOD"] === "POST"){
+    $input= json_decode(file_get_contents("php://input"), true);
+    $res= verifyUser($input);
+    var_dump($res);
+}
 
 echo "Bienvenue sur l'API EcoRide";
